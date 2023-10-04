40c9bfaa81dea8fb98b96269d1a70b31d7e373bc
Prajjwal-creator <139520522+Prajjwal-creator@users.noreply.github.com>
2023-10-04 15:03:38 +0530

    Add files via upload

---
+++ 1.3.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+        homeTitle: 'Welcome to My Portfolio',
+        aboutTitle: 'About Me',
+        aboutContent: 'I am a passionate web developer with expertise in Vue.js, HTML, and CSS.',
+        contactTitle: 'Contact Me',
+        contactContent: 'Email: prajjwalmakkar57@gmail.com'
+    }
+});
